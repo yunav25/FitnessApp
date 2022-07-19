6999f2df0c91f3ad3bc991bdb9507b3f5e7b5208
Manoday1805 <105432345+Manoday1805@users.noreply.github.com>
2022-07-19 16:50:48 +0400

    changes to newWindow

--- WithSplash/NewWindow.java
+++ WithSplash/NewWindow.java
@@ -1,4 +1,8 @@
+
 package WithSplash;
+
+package fitnessAppBranch;
+
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,11 +18,22 @@ public class NewWindow extends JFrame{
 	JTextField Weightfield = new JTextField();
 	JLabel Exerciselabel = new JLabel("Enter Exercise");
 	String exercise[] = {"Push Up", "Pull up", "Sit up", "Squat", "Bench press"};
+
 	//JComboBox combo = new JComboBox(exercise);
 	JButton next = new JButton("Add");
 	JButton submit = new JButton("Submit");
 	//JList<String> exerciseJList = new JList(exercise);
 	
+
+	String[] exercises = {"Push Up\", \"Pull up\", \"Sit up\", \"Squat\", \"Bench press"};
+	
+	JComboBox combo = new JComboBox(exercise);
+	JButton next = new JButton("Add");
+	JButton submit = new JButton("Submit");
+	
+	JList exerciselist = new JList(exercises);
+	JScrollPane jcp = new JScrollPane(exerciselist);
+
 	
 	
 	//constructor of newWindow() class
@@ -26,6 +41,11 @@ public class NewWindow extends JFrame{
 		setLayoutManager();
 		setLocationAndSize();
 		addComponentsToContainer();
+
+
+		exerciselist.setVisibleRowCount(4);
+		
+
 		
 		frame.getContentPane().setBackground(Color.getHSBColor(177, 240, 215));
 		frame.add(usernamelabel);
@@ -35,10 +55,17 @@ public class NewWindow extends JFrame{
 	 	frame.add(Weightlabel);
 	 	frame.add(Weightfield);
 	 	frame.add(Exerciselabel);
+
 	 	//frame.add(combo);
 	 	frame.add(next);
 	 	frame.add(submit);
 	 	
+
+	 	frame.add(combo);
+	 	frame.add(next);
+	 	frame.add(submit);
+	 	frame.add(jcp);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(600,400);
 		frame.setLocationRelativeTo(null);
@@ -51,7 +78,10 @@ public class NewWindow extends JFrame{
 	}
 
 	public void setLocationAndSize(){
+
 		//exerciseJList.setVisibleRowCount(3);
+
+
 		
 		
 		usernamelabel.setBounds(0,0,100,25);
@@ -61,10 +91,17 @@ public class NewWindow extends JFrame{
 		Weightlabel.setBounds(300,0,100,25);
 		Weightfield.setBounds(300,40,100,20);
 		Exerciselabel.setBounds(0,100,100,25);
+
 		//combo.setBounds(0,130,100,20);
 		next.setBounds(145,130,100,25);
 		submit.setBounds(300,230,100,25);
 		//exerciseJList.setBounds(400,130,100,25);
+
+		combo.setBounds(0,130,100,20);
+		next.setBounds(145,130,100,25);
+		submit.setBounds(300,230,100,25);
+		jcp.setBounds(400,130,200,50);
+
 	}
 
 	public void addComponentsToContainer(){
@@ -75,10 +112,42 @@ public class NewWindow extends JFrame{
 		container.add(Weightlabel);
 	 	container.add(Weightfield);
 	 	container.add(Exerciselabel);
+
 	 	//container.add(combo);
 	 	container.add(next);
 	 	container.add(submit);
 	 	//container.add(exerciseJList);
 	}
+
+
+	 	container.add(combo);
+	 	container.add(next);
+	 	container.add(submit);
+	 	container.add(jcp);
+	}
+
+
+
+	// public NewWindow() {
+		
+	// 	//setLayout(new FlowLayout(FlowLayout.CENTER, 20,40));
+	// 	label.setBounds(0,0,100,50);
+		
+	// 	textField = CreateJTextField();
+	// 	frame.add(textField);
+	// 	frame.add(label);
+	// 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	// 	frame.setSize(420,420);
+	// 	frame.setLayout(null);
+	// 	frame.setVisible(true);	
+	// }
+	// public JTextField CreateJTextField() {
+	// 	JTextField textField = new JTextField(15);
+	// 	textfield.setBounds(0,0,100,50);
+		
+	// 	return textField;
+	// }
+	
+
 
 }
